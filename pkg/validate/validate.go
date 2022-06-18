95ebecb757786cf374bf61fc3b739e3b16cbb012
Edsuns <Edsuns@qq.com>
2022-06-18 15:56:12 +0800

    refactor: custom Gin validator

--- pkg/validate/validate.go
+++ pkg/validate/validate.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-playground/locales/zh"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	zhs "github.com/go-playground/validator/v10/translations/zh"
 	"net/http"
 	"strings"
 )
@@ -14,10 +15,22 @@ import (
 var trans ut.Translator
 
 func Setup() {
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		// change tag name
+		v.SetTagName("validate")
+		// setup message translations
+		registerTranslations(v)
+	}
+}
+
+func registerTranslations(v *validator.Validate) {
+	// use Chinese as the default translation
 	ch := zh.New()
 	trans, _ = ut.New(ch, ch).GetTranslator("zh")
-	// replace the default validator with our validator implementation
-	binding.Validator = newValidatorImpl(trans)
+	err := zhs.RegisterDefaultTranslations(v, trans)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func Struct[T any](c *gin.Context, obj *T) *T {
@@ -28,6 +41,7 @@ func StructQuery[T any](c *gin.Context, obj *T) *T {
 	return bind(c, c.ShouldBindQuery, obj)
 }
 
+// bind automatically send errors when parameter errors occur
 func bind[T any](c *gin.Context, bind func(obj interface{}) error, obj *T) *T {
 	if err := bind(obj); err != nil {
 		// if invalid
